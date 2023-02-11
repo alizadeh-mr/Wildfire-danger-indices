ef2f8acbd1d333ee8a9ce107b1649c61e5c6f25b
alizadeh-mr <44510762+alizadeh-mr@users.noreply.github.com>
2023-02-10 22:22:45 -0500

    Update Ext_Areas.js

--- Ext_Areas.js
+++ Ext_Areas.js
@@ -1,74 +1,47 @@
-var Dem = ee.Image("USGS/3DEP/10m"),
-    Ecoregions3_M = ee.FeatureCollection("users/alizadehmr1/Wildfire/Ecoregions_L3_Merged"),
-    Burn_Severity = ee.ImageCollection("users/alizadehmr1/Burn_severity"),
-    Forest = ee.Image("users/alizadehmr1/Wildfire/Forest"),
-    Burned_Areas3 = ee.FeatureCollection("users/alizadehmr1/Wildfire/MTBS_Burned_Areas3"),
-    table = ee.FeatureCollection("users/alizadehmr1/Wildfire/MTBS_Burned_Areas2");
+// Import the necessary data sources
+var Dem = ee.Image("USGS/3DEP/10m");
+var Ecoregions3_M = ee.FeatureCollection("users/alizadehmr1/Wildfire/Ecoregions_L3_Merged");
+var Burn_Severity = ee.ImageCollection("users/alizadehmr1/Burn_severity");
+var Forest = ee.Image("users/alizadehmr1/Wildfire/Forest");
+var Burned_Areas3 = ee.FeatureCollection("users/alizadehmr1/Wildfire/MTBS_Burned_Areas3");
+var table = ee.FeatureCollection("users/alizadehmr1/Wildfire/MTBS_Burned_Areas2");
 
-// Specifing Ecoregion
-var region_dic = ee.Dictionary()
-for (var r=1 ; r<86 ; ++r){
-var Ecoregion = r
-var regions = Ecoregions3_M.filter(ee.Filter.eq("US_L3CODE", Ecoregion.toString()))
-Map.addLayer(regions.draw('red'))
+// Define a dictionary for storing the calculated values for each Ecoregion
+var regionDictionary = ee.Dictionary();
 
-var maskedForest = ee.Image(Forest.eq(0))
-var Forestmasked = maskedForest.updateMask(maskedForest).eq(1)
-var Dem_Forest = Dem.mask(Forestmasked)
-var Mask_Interv_1 = ee.Image(Dem_Forest.gt(ee.Number(0)).and(Dem_Forest.lte(ee.Number(500))));
-var Mask_Interv_2 = ee.Image(Dem_Forest.gt(ee.Number(500)).and(Dem_Forest.lte(ee.Number(1000))));
-var Mask_Interv_3 = ee.Image(Dem_Forest.gt(ee.Number(1000)).and(Dem_Forest.lte(ee.Number(1500))));
-var Mask_Interv_4 = ee.Image(Dem_Forest.gt(ee.Number(1500)).and(Dem_Forest.lte(ee.Number(2000))));
-var Mask_Interv_5 = ee.Image(Dem_Forest.gt(ee.Number(2000)).and(Dem_Forest.lte(ee.Number(2500))));
-var Mask_Interv_6 = ee.Image(Dem_Forest.gt(ee.Number(2500)).and(Dem_Forest.lte(ee.Number(3000))));
-var Mask_Interv_7 = ee.Image(Dem_Forest.gt(ee.Number(3000)));
-var list = ee.List([]);
-///////////////////////////////////////////////////////////////
-var AreaIntrv1 = Mask_Interv_1.multiply(ee.Image.pixelArea())
-var TBA_Interv_1 = AreaIntrv1.reduceRegion({reducer: ee.Reducer.sum(),geometry: regions.geometry(),scale: 300,maxPixels: 1e13})
-var TBA_Interv_1 = ee.Number(TBA_Interv_1.get('elevation')).divide(1e6)
-// print(TBA_Interv_1)
-list = list.add(TBA_Interv_1)
-
-var AreaIntrv2 = Mask_Interv_2.multiply(ee.Image.pixelArea())
-var TBA_Interv_2 = AreaIntrv2.reduceRegion({reducer: ee.Reducer.sum(),geometry: regions.geometry(),scale: 300,maxPixels: 1e13})
-var TBA_Interv_2 = ee.Number(TBA_Interv_2.get('elevation')).divide(1e6)
-// print(TBA_Interv_2)
-list = list.add(TBA_Interv_2)
-
-var AreaIntrv3 = Mask_Interv_3.multiply(ee.Image.pixelArea())
-var TBA_Interv_3 = AreaIntrv3.reduceRegion({reducer: ee.Reducer.sum(),geometry: regions.geometry(),scale: 300,maxPixels: 1e13})
-var TBA_Interv_3 = ee.Number(TBA_Interv_3.get('elevation')).divide(1e6)
-// print(TBA_Interv_3)
-list = list.add(TBA_Interv_3)
-
-var AreaIntrv4 = Mask_Interv_4.multiply(ee.Image.pixelArea())
-var TBA_Interv_4 = AreaIntrv4.reduceRegion({reducer: ee.Reducer.sum(),geometry: regions.geometry(),scale: 300,maxPixels: 1e13})
-var TBA_Interv_4 = ee.Number(TBA_Interv_4.get('elevation')).divide(1e6)
-// print(TBA_Interv_4)
-list = list.add(TBA_Interv_4)
-
-var AreaIntrv5 = Mask_Interv_5.multiply(ee.Image.pixelArea())
-var TBA_Interv_5 = AreaIntrv5.reduceRegion({reducer: ee.Reducer.sum(),geometry: regions.geometry(),scale: 300,maxPixels: 1e13})
-var TBA_Interv_5 = ee.Number(TBA_Interv_5.get('elevation')).divide(1e6)
-// print(TBA_Interv_5)
-list = list.add(TBA_Interv_5)
-
-var AreaIntrv6 = Mask_Interv_6.multiply(ee.Image.pixelArea())
-var TBA_Interv_6 = AreaIntrv6.reduceRegion({reducer: ee.Reducer.sum(),geometry: regions.geometry(),scale: 300,maxPixels: 1e13})
-var TBA_Interv_6 = ee.Number(TBA_Interv_6.get('elevation')).divide(1e6)
-// print(TBA_Interv_6)
-list = list.add(TBA_Interv_6)
-
-var AreaIntrv7 = Mask_Interv_7.multiply(ee.Image.pixelArea())
-var TBA_Interv_7 = AreaIntrv7.reduceRegion({reducer: ee.Reducer.sum(),geometry: regions.geometry(),scale: 300,maxPixels: 1e13})
-var TBA_Interv_7 = ee.Number(TBA_Interv_7.get('elevation')).divide(1e6)
-list = list.add(TBA_Interv_7)
-// }
-region_dic = region_dic.set(r.toString(),list)
-}
-
-print(region_dic)
-Export.table.toDrive(ee.FeatureCollection([ee.Feature(null,region_dic)]));
-
-
+// Loop through each Ecoregion
+for (var r = 1; r < 86; r++) {
+  var Ecoregion = r;
+  var regions = Ecoregions3_M.filter(ee.Filter.eq("US_L3CODE", Ecoregion.toString()));
+  
+  // Add the regions to the map
+  Map.addLayer(regions.draw('red'));
+  
+  // Mask the Forest image
+  var maskedForest = ee.Image(Forest.eq(0));
+  var ForestMasked = maskedForest.updateMask(maskedForest).eq(1);
+  
+  // Mask the Dem image with the masked Forest image
+  var DemForest = Dem.mask(ForestMasked);
+  
+  // Create a list of mask images for different elevation intervals
+  var MaskInterv1 = ee.Image(DemForest.gt(0).and(DemForest.lte(500)));
+  var MaskInterv2 = ee.Image(DemForest.gt(500).and(DemForest.lte(1000)));
+  var MaskInterv3 = ee.Image(DemForest.gt(1000).and(DemForest.lte(1500)));
+  var MaskInterv4 = ee.Image(DemForest.gt(1500).and(DemForest.lte(2000)));
+  var MaskInterv5 = ee.Image(DemForest.gt(2000).and(DemForest.lte(2500)));
+  var MaskInterv6 = ee.Image(DemForest.gt(2500).and(DemForest.lte(3000)));
+  var MaskInterv7 = ee.Image(DemForest.gt(3000));
+  
+  // Create a list for storing the calculated values
+  var list = ee.List([]);
+  
+  // Calculate the total burned area for each elevation interval
+  var AreaIntrv1 = MaskInterv1.multiply(ee.Image.pixelArea());
+  var TBAMaskedInterv1 = AreaIntrv1.reduceRegion({
+    reducer: ee.Reducer.sum(),
+    geometry: regions.geometry(),
+    scale: 300,
+    maxPixels: 1e13
+  });
+  var TBAMaskedInterv1 = ee.Number(TBAMaskedInterv1.get('elevation')).
